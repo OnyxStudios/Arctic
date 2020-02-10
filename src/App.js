d79d35c845e1ee2895d04ae802bda1c0ec2819ac
abused_master <techperson71@gmail.com>
2020-02-10 00:31:53 -0500

    properly named logos

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-		Hello World!
-    </div>
-  );
-}
+export default class App extends React.Component{
 
-export default App;
+    state = {
+    };
+
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
