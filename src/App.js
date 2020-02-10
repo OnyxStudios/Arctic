3efb67e2c1294d52dcbc5c8e58360176dddc8f0e
abused_master <techperson71@gmail.com>
2020-02-10 11:15:58 -0500

    Setup the base layout grid

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+const NavStyles = require('./styles/NavStyles');
 
 export default class App extends React.Component{
 
@@ -7,8 +8,24 @@ export default class App extends React.Component{
 
     render() {
         return (
-            <div>
-                Hello World!
+            <div className='body'>
+                <div style={NavStyles.sideNav}>
+                    <div style={NavStyles.navLogo}>
+                        Logo
+                    </div>
+
+                    Side Navigation
+                </div>
+
+                <div style={NavStyles.rightContent}>
+                    <div style={NavStyles.topNav}>
+                        Top Navigation
+                    </div>
+
+                    <div style={NavStyles.content}>
+                        Panel Content
+                    </div>
+                </div>
             </div>
         );
     }
---
+++ src/styles/NavStyles.js
@@ -0,0 +1,30 @@
+let NavStyles = {
+    sideNav: {
+        backgroundColor: 'red',
+        width: '15%',
+        height: '100%'
+    },
+    navLogo: {
+        width: '100%',
+        height: '8%',
+        backgroundColor: 'green'
+    },
+    rightContent: {
+        width: '85%',
+        height: '100%',
+        display: 'flex',
+        flexDirection: 'column'
+    },
+    topNav: {
+        width: '100%',
+        height: '8%',
+        backgroundColor: 'blue'
+    },
+    content: {
+        width: '100%',
+        height: '92%',
+        backgroundColor: 'pink'
+    }
+};
+
+module.exports = NavStyles;
